181f02a33f498fa54ae57be860f02f98bf8ab211
sblAtGitHub <sbl@http418.de>
2015-12-29 14:49:06 +0100

    Implemented unit-tests for all functions.

---
+++ test.cpp
@@ -0,0 +1,89 @@
+#include "test.hpp"
+
+bool TEST_IsCoPrime() {
+	uint64_t x = 2731;
+	uint64_t y = 764;
+	if(!IsCoPrime(x, y))
+		return false;
+	x = 2732;
+	if(IsCoPrime(x, y))
+		return false;	
+	return true;
+}
+
+bool TEST_StringToBits() {
+	std::vector<int> test {0,1,0,1,1,0,0,0};
+	if(test == StringToBits("X"))
+		return true;
+	return false;
+}
+
+bool TEST_BitsToString() {
+	std::vector<int> test {0,1,0,1,1,0,0,0};
+	if("X" == BitsToString(test))
+		return true;
+	return false;
+}
+
+bool TEST_IncrementVector() {
+	std::vector<int> v1 {0,1,0,1,1,0,0,0};
+	std::vector<int> v2 {0,1,0,1,1,0,0,1};
+	IncrementVector(v1);
+	if(v1 == v2)
+		return true;
+	return false;
+}
+
+bool TEST_GetDotProduct_64() {
+	std::vector<int> v1 {0,1,0,1};
+	std::vector<uint64_t> v2 {1,2,3,4};
+	if(6 == GetDotProduct_64(v1, v2))
+		return true;
+	return false;
+}
+
+bool TEST_GetVector_64() {
+	unsigned int size = 42;
+	std::vector<uint64_t> test = GetVector_64(size);
+	if(size != test.size())
+		return false;
+	std::sort(test.begin(), test.end());
+	for(unsigned int i=0; i<test.size(); ++i) {
+		if(i < (test.size()+1)) {
+			if(test[i] == test[i+1])
+				return false;
+		}
+	}
+	return true;
+}
+
+bool TEST_Encrypt_64() {
+	std::vector<int> message {1,0,0,1};
+	std::vector<uint64_t> key {1,2};
+	std::vector<uint64_t> crypt {1,2};
+	if(crypt == Encrypt_64(message, key))
+		return true;
+	return false;
+}
+
+bool TEST_Break_64() {
+	std::vector<int> message {1,0,0,1};
+	std::vector<uint64_t> key {1,2};
+	std::vector<uint64_t> crypt {1,2};
+	if(message == Break_64(crypt, key))
+		return true;
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.hpp
@@ -0,0 +1,10 @@
+#include "knapsackLib.hpp"
+
+bool TEST_IsCoPrime();
+bool TEST_StringToBits();
+bool TEST_BitsToString();
+bool TEST_IncrementVector();
+bool TEST_GetDotProduct_64();
+bool TEST_GetVector_64();
+bool TEST_Encrypt_64();
+bool TEST_Break_64();
